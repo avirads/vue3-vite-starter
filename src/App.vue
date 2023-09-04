0a496c87e27bd8db5c2b9f04b53aa5416b5e38fe
avirads <avirads@gmail.com>
2023-09-04 13:10:55 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Component0 from './components/Component0.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <Component0 msg="hello world from vite vue" />
+    </div>
+  </header>
+
+  <main>
+    maina
+  </main>
+</template>
+
+<style scoped>
+
+</style>
